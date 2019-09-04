3923d8d63f71bbd97b6b87e6c3e9a6ccad6f3181
Erin Koen <erin.koen@gmail.com>
2019-09-04 14:34:21 -0400

    api framework outlined

--- QuestionTime/QuestionTime/urls.py
+++ QuestionTime/QuestionTime/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+# re_path allows us to redirect based on regex
+from django.urls import include, path, re_path
 # one_step.views eliminates email verification. To learn more:
 # django-registration.readthedocs.io/en/3.0/activation-workflow.html
 from django_registration.backends.one_step.views import RegistrationView
 
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
 
 urlpatterns = [
@@ -45,4 +47,6 @@ urlpatterns = [
     # registration via REST
     path("api/rest-auth/registration/",
          include("rest_auth.registration.urls")),
+
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point ")
 ]
---
+++ QuestionTime/core/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+
+
+class IndexTemplateView(LoginRequiredMixin, TemplateView):
+    # LRMixin means lonly authenticated users can interact with app
+
+    def get_template_names(self):
+        template_name = "index.html"
+        return template_name
